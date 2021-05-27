f3b983f300febb6cd777c249bb1345ed69a2a5b3
何飒 <53459142+13263081188@users.noreply.github.com>
2021-05-28 00:31:28 +0800

    Update app.py

--- demo/app.py
+++ demo/app.py
@@ -7,18 +7,20 @@ import requests
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-import  pandas as pd
+import pandas as pd
 from PIL import Image
 from io import BytesIO
 from torchvision import models
 from torchvision.transforms.functional import resize, to_tensor, normalize, to_pil_image
 from torchcam import cams
 from torchcam.utils import overlay_mask
+
 CAM_METHODS = ["CAM", "GradCAM", "GradCAMpp", "SmoothGradCAMpp", "ScoreCAM", "SSCAM", "ISCAM", "XGradCAM"]
 TV_MODELS = ["resnet18", "resnet50", "mobilenet_v2", "mobilenet_v3_small", "mobilenet_v3_large"]
 LABEL_MAP = requests.get(
     "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
 ).json()
+
 
 # @st.cache
 def main():
@@ -31,9 +33,8 @@ def main():
     test = st.beta_columns(3)
     zz = test[0].form("input image")
     # zz.form_submit_button("dont'touch")
-    
-    
-    #cam_ for i in range(1000)
+
+    # cam_ for i in range(1000)
     cols = [st.form(str(i)) for i in range(len(CAM_METHODS))]
     # cols[0].write("Input image")
     # zz.form_submit_button("DONT'touch me")
@@ -50,7 +51,6 @@ def main():
     DATE_COLUMN = 'date/time'
     DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
                 'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
     @st.cache
     def load_data(nrows):
         data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -74,48 +74,9 @@ def main():
     # Some number in the range 0-23
     hour_to_filter = st.slider('hour', 0, 23, 17)
     filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-    
-    
-    
-    
+
     st.subheader('Map of all pickups at %s:00' % hour_to_filter)
     st.map(filtered_data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     # Sidebar
     # File selection
@@ -124,16 +85,29 @@ def main():
     st.set_option('deprecation.showfileUploaderEncoding', False)
     # Choose your own image
     uploaded_file = st.sidebar.file_uploader("Upload files", type=['png', 'jpeg', 'jpg'])
+    # print(uploaded_file)
     if uploaded_file is not None:
         # zz.write("Aa Aa Aa")
         # st.balloons()
+        #this kind of format is jpeg,<class 'PIL.JpegImagePlugin.JpegImageFile'>
+        # print("Image.open(BytesIO(uploaded_file.read()), mode='r')",type(Image.open(BytesIO(uploaded_file.read()), mode='r')))
+
+        #this kind of format is <class 'bytes'>
+        # print("uploaded_file.read()",
+        #       type(uploaded_file.read()))
+
         img = Image.open(BytesIO(uploaded_file.read()), mode='r').convert('RGB')
-        zz.image(img,use_column_width=True)
+
+        #this kind of format is  <class 'PIL.Image.Image'>
+        # print("IMG",type(img))
+
+        zz.image(img, use_column_width=True)
     if zz.form_submit_button("DONT'touch me"):
         # if uploaded_file is not None:
         # st.balloons()
         pass
-    #Model selection
+
+    # Model selection
     st.sidebar.title("Setup")
     tv_model = st.sidebar.selectbox("Classification model", TV_MODELS)
     default_layer = ""
@@ -156,7 +130,7 @@ def main():
         cam_extractor = cams.__dict__[cam_method](
             model,
             target_layer=target_layer if len(target_layer) > 0 else None
-    )
+        )
     # st.write(cam_method)
     class_choices = [f"{idx + 1} - {class_name}" for idx, class_name in enumerate(LABEL_MAP)]
     class_selection = st.sidebar.selectbox("Class selection", ["Predicted class (argmax)"] + class_choices)
@@ -165,7 +139,7 @@ def main():
     # st.write('\n')
     for i in range(len(CAM_METHODS)):
         # cols[i + 1].form_submit_button("COMPUTE " + CAM_METHODS[i])
-    # for i in range(1,4):
+        # for i in range(1,4):
         if cols[i].form_submit_button("COMPUTE " + CAM_METHODS[i]):
             st.balloons()
             if uploaded_file is None:
@@ -174,7 +148,8 @@ def main():
                 st.balloons()
                 with st.spinner('Analyzing...'):
                     # Preprocess image
-                    img_tensor = normalize(to_tensor(resize(img, (224, 224))), [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    img_tensor = normalize(to_tensor(resize(img, (224, 224))), [0.485, 0.456, 0.406],
+                                           [0.229, 0.224, 0.225])
                     # Forward the image to the model
                     out = model(img_tensor.unsqueeze(0))
                     # Select the target class
@@ -182,30 +157,41 @@ def main():
                         class_idx = out.squeeze(0).argmax().item()
                     else:
                         class_idx = LABEL_MAP.index(class_selection.rpartition(" - ")[-1])
+
                     # Retrieve the CAM
                     activation_map = cam_extractor(class_idx, out)
+                    print("avtivation_map",type(activation_map))
+                    print(activation_map.size())
                     x, y, z = cols[i].beta_columns(3)
                     # Plot the raw heatmap
                     fig, ax = plt.subplots()
                     ax.imshow(activation_map.numpy())
                     ax.axis('off')
                     # cols_1,cols_2,cols_3 = cols[i].beta_columns(3)
-                    x.image(img,use_column_width=True)
+                    x.image(img, use_column_width=True)
                     # y.image(img,use_column_width=True)
                     # cols_1.write('1')
                     # cols_2.write("1")
-                    im = Image.fromarray(activation_map.numpy()).convert('RGB')
-                    y.image(im,use_column_width = True)
+
+                    #im > PIL.Image.Image
+                    im = Image.fromarray(activation_map.numpy())
+                    print(type(im))
+                    y.image(im, use_column_width=True)
                     # Overlayed CAM
                     fig, ax = plt.subplots()
+
+                    #about mode https://blog.csdn.net/u013066730/article/details/102832597
+                    #F represent that grey and pixel value-float 32
                     result = overlay_mask(img, to_pil_image(activation_map, mode='F'), alpha=0.5)
                     ax.imshow(result)
-                    im = result
+                    im = Image.fromarray(result).convert('RGB')
+                    print("result",type(result))
                     ax.axis('off')
                     # cols_3.write("1")
                     # cols_2.pyplot(fig)
                     # z.image(img,use_column_width=True)
-                    z.image(im,use_column_width = True)
+                    z.image(im, use_column_width=True)
+
 
 if __name__ == '__main__':
     main()
